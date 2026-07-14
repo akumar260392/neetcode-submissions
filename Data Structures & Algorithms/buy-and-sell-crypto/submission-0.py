b47d14b5c8eeaa8fa1a8b5734fb77e4d55512272
akumar260392 <97045624+akumar260392@users.noreply.github.com>
2026-07-14 13:02:41 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            buy = min(buy,price)
+            max_profit = max(max_profit,price-buy)
+        return max_profit
+        
